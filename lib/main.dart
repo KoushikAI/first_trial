e79d0fa016948e3e41afdc2eef8ce0f3204c07db
FlutLab.io <>
2021-08-16 12:22:18 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,52 @@ class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.list,
+            ),
+            onPressed: () {},
+          )
+        ],
+        title: Text(
+          "GCTC",
+          textAlign: TextAlign.center,
+        ),
+      ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+          ],
+        ),
+      ),
       backgroundColor: Colors.purple,
       body: Center(
         child: Container(
