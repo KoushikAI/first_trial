a47e52e64b127c3af21880b471f2307003377877
FlutLab.io <>
2021-08-16 11:36:34 +0000

    Autosave

--- lib/FIRST/first.dart
+++ lib/FIRST/first.dart
@@ -11,133 +11,134 @@ class _FirstState extends State<First> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.black12,
         body: Center(
-      child: Container(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          RaisedButton(
-            // Within the `FirstRoute` widget
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => CSE()),
-              );
-            },
-            textColor: Colors.white,
-            color: Colors.blueAccent,
-            disabledColor: Colors.grey,
-            disabledTextColor: Colors.white,
-            highlightColor: Colors.orangeAccent,
-            elevation: 8.0,
-            child: Text(
-              'CSE',
-              style: TextStyle(
-                fontSize: 32.0,
-                color: Colors.redAccent,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-          ),
-          SizedBox(
-            //Use of SizedBox
-            height: 30,
-          ),
-          RaisedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ECE()),
-              );
-            },
-            textColor: Colors.white,
-            color: Colors.blueAccent,
-            disabledColor: Colors.grey,
-            disabledTextColor: Colors.white,
-            highlightColor: Colors.orangeAccent,
-            elevation: 8.0,
-            child: SizedBox(
-              child: Text(
-                'ECE',
-                style: TextStyle(
-                  fontSize: 32.0,
-                  color: Colors.greenAccent,
-                  fontWeight: FontWeight.w500,
+          child: Container(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                // Within the `FirstRoute` widget
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CSE()),
+                  );
+                },
+                textColor: Colors.white,
+                color: Colors.blueAccent,
+                disabledColor: Colors.grey,
+                disabledTextColor: Colors.white,
+                highlightColor: Colors.orangeAccent,
+                elevation: 8.0,
+                child: Text(
+                  'CSE',
+                  style: TextStyle(
+                    fontSize: 32.0,
+                    color: Colors.redAccent,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
               ),
-            ),
-          ),
-          SizedBox(
-            //Use of SizedBox
-            height: 30,
-          ),
-          RaisedButton(
-            onPressed: () {
-              debugPrint('I am Awesome');
-            },
-            textColor: Colors.white,
-            color: Colors.blueAccent,
-            disabledColor: Colors.grey,
-            disabledTextColor: Colors.white,
-            highlightColor: Colors.orangeAccent,
-            elevation: 8.0,
-            child: Text(
-              'EEE',
-              style: TextStyle(
-                fontSize: 32.0,
-                color: Colors.greenAccent,
-                fontWeight: FontWeight.w500,
+              SizedBox(
+                //Use of SizedBox
+                height: 30,
               ),
-            ),
-          ),
-          SizedBox(
-            //Use of SizedBox
-            height: 30,
-          ),
-          RaisedButton(
-            onPressed: () {
-              debugPrint('I am Awesome');
-            },
-            textColor: Colors.white,
-            color: Colors.blueAccent,
-            disabledColor: Colors.grey,
-            disabledTextColor: Colors.white,
-            highlightColor: Colors.orangeAccent,
-            elevation: 8.0,
-            child: Text(
-              'ME',
-              style: TextStyle(
-                fontSize: 32.0,
-                color: Colors.greenAccent,
-                fontWeight: FontWeight.w500,
+              RaisedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ECE()),
+                  );
+                },
+                textColor: Colors.white,
+                color: Colors.blueAccent,
+                disabledColor: Colors.grey,
+                disabledTextColor: Colors.white,
+                highlightColor: Colors.orangeAccent,
+                elevation: 8.0,
+                child: SizedBox(
+                  child: Text(
+                    'ECE',
+                    style: TextStyle(
+                      fontSize: 32.0,
+                      color: Colors.greenAccent,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
               ),
-            ),
-          ),
-          SizedBox(
-            //Use of SizedBox
-            height: 30,
-          ),
-          RaisedButton(
-            onPressed: () {
-              debugPrint('I am Awesome');
-            },
-            textColor: Colors.white,
-            color: Colors.blueAccent,
-            disabledColor: Colors.grey,
-            disabledTextColor: Colors.white,
-            highlightColor: Colors.orangeAccent,
-            elevation: 8.0,
-            child: Text(
-              'CE',
-              style: TextStyle(
-                fontSize: 32.0,
-                color: Colors.greenAccent,
-                fontWeight: FontWeight.w500,
+              SizedBox(
+                //Use of SizedBox
+                height: 30,
               ),
-            ),
-          ),
-        ],
-      )),
-    ));
+              RaisedButton(
+                onPressed: () {
+                  debugPrint('I am Awesome');
+                },
+                textColor: Colors.white,
+                color: Colors.blueAccent,
+                disabledColor: Colors.grey,
+                disabledTextColor: Colors.white,
+                highlightColor: Colors.orangeAccent,
+                elevation: 8.0,
+                child: Text(
+                  'EEE',
+                  style: TextStyle(
+                    fontSize: 32.0,
+                    color: Colors.greenAccent,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              SizedBox(
+                //Use of SizedBox
+                height: 30,
+              ),
+              RaisedButton(
+                onPressed: () {
+                  debugPrint('I am Awesome');
+                },
+                textColor: Colors.white,
+                color: Colors.blueAccent,
+                disabledColor: Colors.grey,
+                disabledTextColor: Colors.white,
+                highlightColor: Colors.orangeAccent,
+                elevation: 8.0,
+                child: Text(
+                  'ME',
+                  style: TextStyle(
+                    fontSize: 32.0,
+                    color: Colors.greenAccent,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              SizedBox(
+                //Use of SizedBox
+                height: 30,
+              ),
+              RaisedButton(
+                onPressed: () {
+                  debugPrint('I am Awesome');
+                },
+                textColor: Colors.white,
+                color: Colors.blueAccent,
+                disabledColor: Colors.grey,
+                disabledTextColor: Colors.white,
+                highlightColor: Colors.orangeAccent,
+                elevation: 8.0,
+                child: Text(
+                  'CE',
+                  style: TextStyle(
+                    fontSize: 32.0,
+                    color: Colors.greenAccent,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ],
+          )),
+        ));
   }
 }
