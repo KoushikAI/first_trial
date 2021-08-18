71cca197bacbadecc8b0abfabe9c8c7bef4e940f
FlutLab.io <>
2021-08-18 11:43:51 +0000

    Autosave

--- lib/FIRST/first.dart
+++ lib/FIRST/first.dart
@@ -37,6 +37,82 @@ class _FirstState extends State<First> {
             textAlign: TextAlign.center,
           ),
         ),
+        drawer: Drawer(
+          // Add a ListView to the drawer. This ensures the user can scroll
+          // through the options in the drawer if there isn't enough vertical
+          // space to fit everything.
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.yellowAccent,
+                ),
+                child: Text('Notes'),
+              ),
+              ExpansionTile(
+                title: Text("First year"),
+                children: <Widget>[
+                  ButtonTheme(
+                    minWidth: 310,
+                    height: 40,
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                      // Within the `FirstRoute` widget
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => CSE()),
+                        );
+                      },
+                      textColor: Colors.black,
+                      color: Colors.white,
+                      disabledColor: Colors.grey,
+                      disabledTextColor: Colors.white,
+                      highlightColor: Colors.orangeAccent,
+                      elevation: 8.0,
+                      child: Text(
+                        'CSE',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              ListTile(
+                title: const Text('First year'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+              ListTile(
+                title: const Text('Second year'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+              ListTile(
+                title: const Text('Third year'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+              ListTile(
+                title: const Text('Fourth year'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+            ],
+          ),
+        ),
         backgroundColor: Colors.white,
         body: Center(
           child: Container(
